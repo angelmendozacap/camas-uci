71b740419eb3b9a52cb1c4d9d5632050f62485f1
Alfredo Mendoza <1930.kick@gmail.com>
2021-02-01 10:54:59 -0500

    Add Hospital migration

--- database/migrations/2021_02_01_025710_create_hospitals_table.php
+++ database/migrations/2021_02_01_025710_create_hospitals_table.php
@@ -15,6 +15,29 @@ class CreateHospitalsTable extends Migration
     {
         Schema::create('hospitals', function (Blueprint $table) {
             $table->id();
+            $table->string('institution',254);
+            $table->string('name', 254);
+            $table->string('classification', 254);
+            $table->string('type', 254);
+            $table->string('department', 254);
+            $table->string('province', 254);
+            $table->string('district', 254);
+            $table->string('ubigeo', 254);
+            $table->string('address', 254);
+            $table->string('disa', 254);
+            $table->string('red', 254)->nullable();
+            $table->string('micro_red', 254)->nullable();
+            $table->string('executor', 254)->nullable();
+            $table->string('category', 254)->nullable();
+            $table->string('phone', 254)->nullable();
+            $table->date('started_at', 254);
+            $table->string('responsible', 254)->nullable();
+            $table->boolean('status');
+            $table->unsignedTinyInteger('condition', 254);
+            $table->decimal('north', 19, 15);
+            $table->decimal('east', 19, 15);
+            $table->decimal('height', 19, 15);
+            $table->unsignedInteger('beds_quantity', 254);
             $table->timestamps();
         });
     }
